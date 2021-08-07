dc18f2a52ecd22fd0fa740fc8c45a0c3ec728770
Mikey <mikeywearing@gmail.com>
2021-08-07 15:39:16 +0200

    First Commit

---
+++ FizzBuzzer/Single.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FizzBuzzer
+{
+    public class Single
+    {
+        public string FizzThis(int number)
+        {
+            if (isFizzBuzz(number))
+            {
+                return "FizzBuzz";
+            }
+            else if (IsFizz(number))
+            {
+                return "Fizz";
+            }
+            else if (IsBuzz(number))
+            {
+                return "Buzz";
+            }
+            return number.ToString();
+        }
+
+        private static bool isFizzBuzz(int number) => number % 3 == 0 && number % 5 == 0;
+
+        private static bool IsBuzz(int number) => number % 5 == 0;
+
+        private static bool IsFizz(int number) => number % 3 == 0;
+    }
+}
